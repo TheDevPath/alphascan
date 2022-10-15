286419105459ae6aa805dff3a5785ee2d78501d2
Bryan Sharpley <motosharpley@gmail.com>
2022-10-15 13:25:44 -0700

    set up form and fetch component state api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,61 @@
-import logo from './dplogo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [resourceAddress, setResourceAddress] = useState('');
+  const [results, setResults] = useState(
+    'Enter Resource Address To Fetch Data'
+  );
+
+  const fetchResults = async (e) => {
+    e.preventDefault();
+    console.log('fetching ledger state');
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    var raw = JSON.stringify({
+      component_address: resourceAddress,
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    fetch('https://alphanet.radixdlt.com/v0/state/component', requestOptions)
+      .then((response) => response.text())
+      .then((result) => setResults(result))
+      .catch((error) => console.log('error', error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://youtube.com/playlist?list=PL4EVBVoyp_YCzFkrH6ziVT70wuh0o-lCW"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Scrypto
-        </a>
-      </header>
+      <form onSubmit={fetchResults}>
+        <label>
+          <span>Resource Address:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setResourceAddress(e.target.value)}
+            value={resourceAddress}
+          />
+        </label>
+        <hr />
+        <button type="submit">Get Resource Data</button>
+      </form>
+
+      <div className="results">{results}</div>
+      <a
+        className="App-link"
+        href="https://youtube.com/playlist?list=PL4EVBVoyp_YCzFkrH6ziVT70wuh0o-lCW"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Scrypto
+      </a>
     </div>
   );
 }
